413bb4fa0eafaf41df6e435e63c848a784857a31
CherryPerry <ya.cherryperry@yandex.ru>
2017-02-23 17:20:56 +0300

    Scripts update

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-./gradlew jar
-rm /var/amiami/app.jar
+./gradlew build
 mkdir /var/amiami
-mv build/libs/amiami-backend-1.0.jar /var/amiami/app.jar
-rm /etc/systemd/system/amiami.service
-mv service.systemd /etc/systemd/system/amiami.service
+chmod +x build/libs/amiami-backend-1.0.jar
+cp build/libs/amiami-backend-1.0.jar /var/amiami/app.jar
+cp service.systemd /etc/systemd/system/amiami.service
 systemctl enable amiami.service
 systemctl daemon-reload
 systemctl restart amiami.service
--- update_build_and_install.sh
+++ update_build_and_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 ./gradlew build
-rm /var/amiami/app.jar
 chmod +x build/libs/amiami-backend-1.0.jar
-mv build/libs/amiami-backend-1.0.jar /var/amiami/app.jar
+cp build/libs/amiami-backend-1.0.jar /var/amiami/app.jar
 systemctl daemon-reload
 systemctl restart amiami.service
