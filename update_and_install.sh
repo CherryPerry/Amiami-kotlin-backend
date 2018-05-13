ede7dffd27e345df920525abdbe104ee9e220d05
CherryPerry <ya.cherryperry@yandex.ru>
2018-05-13 17:33:53 +0300

    Use gradle task to do all installation job

--- update_and_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./gradlew build
-chmod +x build/libs/amiami-backend-1.1.jar
-cp build/libs/amiami-backend-1.1.jar /var/amiami/app.jar
-systemctl daemon-reload
-systemctl restart amiami.service
