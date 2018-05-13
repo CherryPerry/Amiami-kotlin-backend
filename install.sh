72341a3052ef6d220ab7c679c0fde4f249925f63
CherryPerry <ya.cherryperry@yandex.ru>
2018-05-13 14:16:04 +0300

    Update version in scripts

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 ./gradlew build
 mkdir /var/amiami
-chmod +x build/libs/amiami-backend-1.0.jar
-cp build/libs/amiami-backend-1.0.jar /var/amiami/app.jar
+chmod +x build/libs/amiami-backend-1.1.jar
+cp build/libs/amiami-backend-1.1.jar /var/amiami/app.jar
 cp service.systemd /etc/systemd/system/amiami.service
 systemctl enable amiami.service
 systemctl daemon-reload
--- update_and_install.sh
+++ update_and_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./gradlew build
-chmod +x build/libs/amiami-backend-1.0.jar
-cp build/libs/amiami-backend-1.0.jar /var/amiami/app.jar
+chmod +x build/libs/amiami-backend-1.1.jar
+cp build/libs/amiami-backend-1.1.jar /var/amiami/app.jar
 systemctl daemon-reload
 systemctl restart amiami.service
